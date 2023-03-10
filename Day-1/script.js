9e95eb1b6014a6318506102fa50736b540145171
Maynul-portfolio <studymaynulislam@gmail.com>
2023-03-10 09:27:09 +0600

    stop the courch start learing js 10/03/23 tme morning 9:26

---
+++ Day-1/script.js
@@ -0,0 +1,17 @@
+const panel = document.querySelectorAll('.panel')
+
+panel.forEach((panel) => { 
+    
+});
+
+
+
+// const number = [12,46,799,345,243]
+
+// function myfuncation(valu,index,array){
+
+//    document.write(valu);
+//    document.write(index);
+//    document.write(array)
+// }
+// number.forEach(myfuncation);
